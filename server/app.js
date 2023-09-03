4e5a07d9263c90e7dbdcc5aab9cd38d161e11fbc
yousufislam191 <yousufislam337@gmail.com>
2023-09-03 19:13:56 +0600

    setup xss protection

--- server/app.js
+++ server/app.js
@@ -3,16 +3,26 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 const createError = require("http-errors");
+const xssClean = require("xss-clean");
+const rateLimit = require("express-rate-limit");
 const app = express();
 
 require("./config/db");
 const userRouter = require("./routes/user.routes");
+
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minutes
+  max: 5, // Limit each IP to 5 requests per `window` (here, per 15 minutes)
+  message: "Too many requests from this IP. Please try again later",
+});
 
 app.use(cors({ credentials: true, origin: "http://127.0.0.1:5173/" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
+app.use(xssClean());
+app.use(limiter);
 
 app.use("/api/user", userRouter);
 
